9199b3536f57f112b7316491a1ada5f52ad6de2a
figatix <60873339+figatix@users.noreply.github.com>
2022-12-13 00:12:45 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,30 @@
+/*
+Завдання 2
+HTML містить порожній список ul#ingredients.
+
+<ul id="ingredients"></ul>
+
+JavaScript містить масив рядків.
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+? Напиши скрипт, який для кожного елемента масиву ingredients:
+
+? 1) Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+? 2) Додасть назву інгредієнта як його текстовий вміст.
+? 3) Додасть елементу клас item.
+? 4) Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+*/
+
+
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +33,31 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const arrayIngredients = []
+
+ingredients.forEach(el => {
+  const itemEl = document.createElement('li');
+
+  itemEl.innerHTML = `${el}`;
+  itemEl.classList.add('item');
+  arrayIngredients.push(itemEl)
+});
+
+ingredientsEl.append(...arrayIngredients);
+
+
+// const arrayIngredients = ingredients.reduce((acc, el) => {
+//   const itemEl = document.createElement('li');
+  
+//   itemEl.innerHTML = `${el}`;
+//   itemEl.classList.add('item');
+
+//   return [...acc, itemEl];
+// }, []);
+
+// console.log(arrayIngredients);
+
+// ingredientsEl.append(...arrayIngredients);
