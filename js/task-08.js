423830dcf5bcf02714fd6cfb32a27d87ea5fe813
figatix <60873339+figatix@users.noreply.github.com>
2022-12-16 23:44:12 +0200

    8,9,10

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,62 @@
+/*
+  Завдання 8
+Напиши скрипт управління формою логіна.
+
+<form class="login-form">
+  <label>
+    Email
+    <input type="email" name="email" />
+  </label>
+  <label>
+    Password
+    <input type="password" name="password" />
+  </label>
+  <button type="submit">Login</button>
+</form>
+
+? Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+? Під час відправлення форми сторінка не повинна перезавантажуватися.
+? Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+? Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+? Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+*/
+
+const formEl = document.querySelector('.login-form')
+const emailInputEl = document.querySelector('[type="email"]')
+const passwordInputEl = document.querySelector('[type="password"]')
+const btntEl = document.querySelector('[type="submit"]')
+
+function onSendForm(event) {
+  event.preventDefault()
+
+  if (emailInputEl.value.trim() === '' || passwordInputEl.value.trim() === '') {
+    alert('Всі поля повинні бути заповнені')
+  }
+
+  const {elements: { email, password }} = event.currentTarget;
+  // console.log(event.currentTarget) 
+  const userObj = {
+    email : email.value,
+    password : password.value,
+  }
+
+  console.log('User object', userObj);
+  // console.log(email.value, password.value);
+  formEl.reset()
+
+}
+
+
+formEl.addEventListener('submit', onSendForm)
+
+
+
+
+
+
+
+
+
+
+
 
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,68 @@
+/*
+  Завдання 10 (виконувати не обов'язково)
+? Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
+
+<div id="controls">
+  <input type="number" min="1" max="100" step="1" />
+  <button type="button" data-create>Create</button>
+  <button type="button" data-destroy>Destroy</button>
+</div>
+
+<div id="boxes"></div>
+
+? Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+?Розміри найпершого <div> - 30px на 30px.
+?Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+?Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+? Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+*/
+
+
+const inputEl = document.querySelector('input')
+const createEl = document.querySelector('[data-create]')
+const destroyEl = document.querySelector('[data-destroy]')
+const boxesEl = document.querySelector('#boxes')
+
+let boxSize = 30;
+
+function createBoxes() {
+  const amount = Number(inputEl.value);
+
+  if (!inputEl.value) {
+    return
+  }
+
+  for (let i = 0; i < amount; i += 1){
+    const divElem = document.createElement('div')
+
+    divElem.style = `background-color: ${getRandomHexColor()}; width: ${boxSize}px; height:${boxSize}px; margin: 10px; `;
+    divElem.classList.add('new-box')
+    
+    boxesEl.append(divElem);
+    boxSize += 10;
+  }
+  
+}
+
+function destroyBoxes() {
+  const newBoxes = document.querySelectorAll('.new-box');
+
+  newBoxes.forEach(element => {
+    element.remove()
+  });
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+createEl.addEventListener('click', createBoxes)
+destroyEl.addEventListener('click', destroyBoxes)
